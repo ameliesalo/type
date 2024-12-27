9b411a77049178fc66154912ce5a9f8f41e5ebad
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-12-27 12:17:33 +0100

    two

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const Base_Url = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
+const ulElement = document.querySelector('.book-list');
 function fetchData(Base_Url) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -17,114 +18,19 @@ function fetchData(Base_Url) {
             }
             const data = yield response.json();
             console.log(data);
-            const book1 = document.querySelector('.book1');
-            book1.innerHTML = `<h2>${data[0].title}<h2>`;
-            book1.addEventListener('click', () => {
-                book1.innerHTML =
-                    `<ul> 
-        <li>${data[0].author}</li>
-        <li>Audience: ${data[0].audience}</li>
-        <li>${data[0].pages} pages</li>
-        <li>${data[0].plot}</li>
-        <li>Released year: ${data[0].year}</li>
-        </ul>`;
-            });
-            book1.addEventListener('keydown', () => { book1.innerHTML = `<h2>${data[0].title}<h2>`; });
-            const book2 = document.querySelector('.book2');
-            book2.innerHTML = `<h2>${data[1].title}<h2>`;
-            book2.addEventListener('click', () => {
-                book2.innerHTML =
-                    `<ul> 
-        <li>${data[1].author}</li>
-        <li>Audience: ${data[1].audience}</li>
-        <li>${data[1].pages} pages</li>
-        <li>${data[1].plot}</li>
-        <li>Released year: ${data[1].year}</li>
-        </ul>`;
-            });
-            book2.addEventListener('keydown', () => { book2.innerHTML = `<h2>${data[1].title}<h2>`; });
-            const book3 = document.querySelector('.book3');
-            book3.innerHTML = `<h2>${data[2].title}<h2>`;
-            book3.addEventListener('click', () => {
-                book3.innerHTML =
-                    `<ul> 
-        <li>${data[2].author}</li>
-        <li>Audience: ${data[2].audience}</li>
-        <li>${data[2].pages} pages</li>
-        <li>${data[2].plot}</li>
-        <li>Released year: ${data[2].year}</li>
-        </ul>`;
-            });
-            book3.addEventListener('keydown', () => { book3.innerHTML = `<h2>${data[2].title}<h2>`; });
-            const book4 = document.querySelector('.book4');
-            book4.innerHTML = `<h2>${data[3].title}<h2>`;
-            book4.addEventListener('click', () => {
-                book4.innerHTML =
-                    `<ul> 
-        <li>${data[3].author}</li>
-        <li>Audience: ${data[3].audience}</li>
-        <li>${data[3].pages} pages</li>
-        <li>${data[3].plot}</li>
-        <li>Released year: ${data[3].year}</li>
-        </ul>`;
-            });
-            book4.addEventListener('keydown', () => { book4.innerHTML = `<h2>${data[3].title}<h2>`; });
-            const book5 = document.querySelector('.book5');
-            book5.innerHTML = `<h2>${data[4].title}<h2>`;
-            book5.addEventListener('click', () => {
-                book5.innerHTML =
-                    `<ul> 
-        <li>${data[4].author}</li>
-        <li>Audience: ${data[4].audience}</li>
-        <li>${data[4].pages} pages</li>
-        <li>${data[4].plot}</li>
-        <li>Released year: ${data[4].year}</li>
-        </ul>`;
-            });
-            book5.addEventListener('keydown', () => { book5.innerHTML = `<h2>${data[4].title}<h2>`; });
-            const book6 = document.querySelector('book6');
-            book6.innerHTML = `<h2>${data[5].title}<h2>`;
-            book6.addEventListener('click', () => {
-                book6.innerHTML =
-                    `<ul> 
-        <li>${data[5].author}</li>
-        <li>Audience: ${data[5].audience}</li>
-        <li>${data[5].pages} pages</li>
-        <li>${data[5].plot}</li>
-        <li>Released year: ${data[5].year}</li>
-        </ul>`;
-            });
-            book6.addEventListener('keydown', () => { book6.innerHTML = `<h2>${data[5].title}<h2>`; });
-            const book7 = document.querySelector('.book7');
-            book7.innerHTML = `<h2>${data[6].title}<h2>`;
-            book7.addEventListener('click', () => {
-                book7.innerHTML =
-                    `<ul> 
-        <li>${data[6].author}</li>
-        <li>Audience: ${data[6].audience}</li>
-        <li>${data[6].pages} pages</li>
-        <li>${data[6].plot}</li>
-        <li>Released year: ${data[6].year}</li>
-        </ul>`;
-            });
-            book7.addEventListener('keydown', () => { book7.innerHTML = `<h2>${data[6].title}<h2>`; });
-            const book8 = document.querySelector('.book8');
-            book8.innerHTML = `<h2>${data[7].title}<h2>`;
-            book8.addEventListener('click', () => {
-                book8.innerHTML =
-                    `<ul> 
-        <li>${data[7].author}</li>
-        <li>Audience: ${data[7].audience}</li>
-        <li>${data[7].pages} pages</li>
-        <li>${data[7].plot}</li>
-        <li>Released year: ${data[7].year}</li>
-        </ul>`;
-            });
-            book8.addEventListener('keydown', () => { book8.innerHTML = `<h2>${data[7].title}<h2>`; });
+            for (let i = 0; i >= data.length; i++) {
+                const liElement = document.createElement('li');
+                liElement.textContent = data[i].titel;
+                const infoElement = document.createElement('div');
+                infoElement.className = 'book-info';
+                infoElement.textContent = 'Författare: ' + `${data[i].författare}`, 'Antal sidor: ' + `${data[i].sidor}`;
+                liElement.addEventListener('click', function () {
+                    infoElement.style;
+                });
+            }
         }
         catch (error) {
-            console.error("Oops!", error);
+            console.error('error!', error);
         }
     });
 }
-fetchData(Base_Url);
